fc67f3a3eb8ff23bddbfd32e2af0e28d05cdcad4
abes400 <125301699+abes400@users.noreply.github.com>
2025-01-27 21:03:23 +0300

    INITIALIZE

---
+++ A-Maze-Ing Ball/Assets/Objects/Maze/Scripts/Rotate.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    
+    [SerializeField] float rotationAngle   = 30.0f;
+
+    [SerializeField] float rotationTime    = 00.2f;
+
+                     float currentVelocity = 00.0f;
+                     float targetAngle     = 00.0f;
+                     
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            targetAngle += rotationAngle;
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+            targetAngle -= rotationAngle;
+
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.z, targetAngle, ref currentVelocity, rotationTime);
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+        
+    }
+}
