439a3952ca8f8ef4266b3f1091ac4cab27d2d808
abes400 <125301699+abes400@users.noreply.github.com>
2025-08-10 19:48:43 +0300

    UPDATE `MainMenu`

---
+++ A-Maze-Ing Ball/Assets/Managers/UI/Scripts/TitleMenus.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class TitleMenus : MonoBehaviour
+{
+    [SerializeField] GameObject Logo;
+    [SerializeField] GameObject Byline;
+    [SerializeField] GameObject MainMenu;
+    [SerializeField] GameObject LevelMenu;
+    [SerializeField] GameObject InstructionsMenu;
+    [SerializeField] GameObject OptionsMenu;
+    [SerializeField] GameObject AboutMenu;
+    [SerializeField] GameObject QuitPopup;
+    public void GameStart()
+    {
+        MainMenuSetActive(false);
+        LevelMenu.SetActive(true);
+    }
+
+    public void Instructions()
+    {
+        MainMenuSetActive(false);
+        InstructionsMenu.SetActive(true);
+    }
+
+    public void Options()
+    {
+        MainMenuSetActive(false);
+        OptionsMenu.SetActive(true);
+    }
+
+    public void About()
+    {
+        MainMenuSetActive(false);
+        AboutMenu.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        MainMenuSetActive(false);
+        QuitPopup.SetActive(true);
+    }
+
+    public void Back()
+    {
+        //LevelMenu.SetActive(false);
+        InstructionsMenu.SetActive(false);
+        OptionsMenu.SetActive(false);
+        AboutMenu.SetActive(false);
+        //QuitPopup.SetActive(false);
+
+        MainMenuSetActive(true);
+    }
+
+
+    private void MainMenuSetActive(bool active)
+    {
+        Logo.SetActive(active);
+        Byline.SetActive(active);
+        MainMenu.SetActive(active);
+    }
+}
