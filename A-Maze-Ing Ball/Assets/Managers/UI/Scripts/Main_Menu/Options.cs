1b6a6ea9f800fa613f1f53c5553e461e77f1cbaa
abes400 <125301699+abes400@users.noreply.github.com>
2025-08-12 03:29:12 +0300

    ADD Fullscreen Logic

--- A-Maze-Ing Ball/Assets/Managers/UI/Scripts/Main_Menu/Options.cs
+++ A-Maze-Ing Ball/Assets/Managers/UI/Scripts/Main_Menu/Options.cs
@@ -1,20 +1,44 @@
+using System;
 using TMPro;
 using UnityEngine;
 
 public class Options : MonoBehaviour
 {
     [SerializeField] GameObject fullScreenButton;
-    [SerializeField] GameObject resCalButton;
+    TextMeshProUGUI buttonText;
+
+    void OnEnable()
+    {
+        buttonText = fullScreenButton.GetComponentInChildren<TextMeshProUGUI>();
+        buttonText.text = $"Fullscreen: {(Screen.fullScreen ? "ON" : "OFF")}";
+    }
 
     public void ToggleFullScreen()
     {
-        Screen.fullScreen = !Screen.fullScreen;
-        fullScreenButton.GetComponentInChildren<TextMeshProUGUI>().text
-            = $"Fullscreen: {(Screen.fullScreen ? "ON" : "OFF")}";
+        bool fullScreenEnabled = !Screen.fullScreen;
+        buttonText.text = $"Fullscreen: {(fullScreenEnabled ? "ON" : "OFF")}";
+        SetFullScreen(fullScreenEnabled);
     }
 
-    public void CalibrateRes()
+    public static void SetFullScreen(bool fullScreenEnabled)
     {
-        
+        int newWidth, newHeight;
+        FullScreenMode newFSMode;
+
+        if (fullScreenEnabled)
+        {
+            newWidth = Display.main.systemWidth;
+            newHeight = Display.main.systemHeight;
+            newFSMode = FullScreenMode.FullScreenWindow;
+        }
+        else
+        {
+            newWidth = (int)(Display.main.systemWidth * 0.8f);
+            newHeight = (int)(Display.main.systemHeight * 0.8f);
+            newFSMode = FullScreenMode.Windowed;
+        }
+
+        Screen.SetResolution(newWidth, newHeight, newFSMode);
+        PlayerPrefs.SetInt("Fullscreen", fullScreenEnabled ? 1 : 0);
     }
 }
